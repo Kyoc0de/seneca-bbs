531362a4e783779ea75187e60f3346cfd73ae2ea
kyo <kyooo9@outlook.com>
2021-01-01 19:15:15 -0500

    header-v1

---
+++ src/common/local-data.js
@@ -0,0 +1,26 @@
+export const headerLinks = [
+  {
+      title: "新闻",
+      link: "/new"
+  },
+  {
+      title: "论坛",
+      link: "/bbs"
+  },
+  {
+      title: "活动",
+      link: "/active"
+  },
+  {
+      title: "官网",
+      link: "https://www.senecacollege.ca/home.html"
+  },
+  {
+      title: "My.Seneca",
+      link: "https://my.senecacollege.ca/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_15_1"
+  },
+  {
+      title: "关于",
+      link: "https://music.163.com/#/download"
+  }
+]
--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,7 +1,49 @@
 import React, { memo } from 'react'
+import { NavLink } from 'react-router-dom' 
+
+import { headerLinks } from '@/common/local-data'
+
+import {
+  HeaderWrapper,
+  HeaderLeft
+} from './style'
 
 export default memo(function KYOAppHeader(){
+
+  //业务代码
+  const showSelectItem = (item, index) => {
+    if(index<3){
+      return(
+        <NavLink to={item.link}>
+          {item.title}
+
+        </NavLink>
+      )
+    }else {
+      return <a href={item.link}>{item.title}</a>
+    }
+  }
+
+  //jsx
   return (
-    <div>Test</div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href='#/' className="logo sprite_01">Seneca中文社区</a>
+          <div className="select-list">
+            {
+              headerLinks.map((item,index)=>{
+                return (
+                  <div key={item.title} className="select-item">
+                    {showSelectItem(item,index)}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </HeaderLeft>
+      </div>
+    </HeaderWrapper>
+
   )
 })
